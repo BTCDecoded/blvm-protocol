fc757b8af4c3e87caa5ec65143f5daf4e5697f93
SecSov <securesovereigns@pm.me>
2025-11-19 21:30:32 +0000

    Update .gitignore: remove target/ and Cargo.lock from tracking
    - Remove 515 build artifacts from git
    - Libraries should not track Cargo.lock

--- target/debug/deps/protocol_engine-0c33f8fce9d31ad8.d
+++
@@ -1,8 +0,0 @@
-/home/user/src/BTCDecoded/protocol-engine/target/debug/deps/protocol_engine-0c33f8fce9d31ad8.d: src/lib.rs src/variants.rs src/validation.rs src/network_params.rs
-
-/home/user/src/BTCDecoded/protocol-engine/target/debug/deps/protocol_engine-0c33f8fce9d31ad8: src/lib.rs src/variants.rs src/validation.rs src/network_params.rs
-
-src/lib.rs:
-src/variants.rs:
-src/validation.rs:
-src/network_params.rs:
